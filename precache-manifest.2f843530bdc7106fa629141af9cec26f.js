62230b66be49def48805eeef4df46d6da78e6ce6
rooky6 <rooky6@users.noreply.github.com>
2021-02-13 11:13:04 -0300

    Updates

---
+++ precache-manifest.2f843530bdc7106fa629141af9cec26f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f72b5a6ca988f52b1afc9ae578d047b",
+    "url": "/pizzajoint/index.html"
+  },
+  {
+    "revision": "7e591888d5533bdeeb46",
+    "url": "/pizzajoint/static/css/main.28732309.chunk.css"
+  },
+  {
+    "revision": "9b86da7f495c77a3c49e",
+    "url": "/pizzajoint/static/js/2.921ef795.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/pizzajoint/static/js/2.921ef795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e591888d5533bdeeb46",
+    "url": "/pizzajoint/static/js/main.e1d2e6ec.chunk.js"
+  },
+  {
+    "revision": "4d9bb7780ce4c05c8554",
+    "url": "/pizzajoint/static/js/runtime-main.635c2d35.js"
+  }
+]);
